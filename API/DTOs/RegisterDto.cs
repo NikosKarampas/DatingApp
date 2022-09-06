d63ecc90c8afc350c3abeccbb5681c0b3cf27649
Nikos Karampas <nikos.karampas@hotmail.com>
2022-09-06 14:38:07 +0300

    End of section 16

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,11 +1,7 @@
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace API.DTOs
-{    
+{
     public class RegisterDto
     {
         [Required] public string Username { get; set; }
@@ -16,7 +12,7 @@ namespace API.DTOs
         [Required] public string Country { get; set; }
 
         [Required]
-        [StringLength(8, MinimumLength = 4)]
+        [StringLength(9, MinimumLength = 4)]
         public string Password { get; set; }
     }
 }
